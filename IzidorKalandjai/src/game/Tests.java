dd81db6a1bb9743d21ac62ca75e25e2b0d025f12
horvath.horadam <horvath.horadam@2d66b122-5dff-8467-672b-2567ea858774>
2012-03-18 18:40:18 +0000

    Megírtam egy console-ról bekérő szám metódusát. Addig kéri be a számokat amíg nem megfelelő tehát amíg nem 1 és 5 között van/ennyi tesztesetünk van/. Ezután írtam egy Tests osztályt, ebben megírtam a tesztesetek vázát(semmi konkrét csak 5 függvény ami meghívásakor, kiírja a consolera h engem hívtak meg...)Main függvényben az Integer beolvasása után switch case szerkezettel meghívodik az adott teszteset. Ádám

---
+++ IzidorKalandjai/src/game/Tests.java
@@ -0,0 +1,33 @@
+package game;
+//teszteseteket tartalmazó osztály, 
+//5 különböző teszteset(kulcsfelvétele majd pálya elhagyása,tilitoli,két tile közötti áthaladás, meghalás, ütközés)
+public class Tests
+{
+	//konstrukor
+	public Tests(){}
+	//Kulcsfelvétele, majd a pálya elhagyásának tesztjének függvénye
+	public void Test1()
+	{
+		System.out.println("I am the first test method");
+	}
+	//Tilitoli tesztjének függvénye
+	public void Test2()
+	{
+		System.out.println("I am the second test method");
+	}
+	//Két tile közötti áthaladás tesztjének a függvénye
+	public void Test3()
+	{
+		System.out.println("I am the third test method");
+	}
+	//Meghalás teszjének a függvénye
+	public void Test4()
+	{
+		System.out.println("I am the fourth test method");
+	}
+	//Ütközés tesztjének a meghívása
+	public void Test5()
+	{
+		System.out.println("I am the fifth test method");
+	}
+}
