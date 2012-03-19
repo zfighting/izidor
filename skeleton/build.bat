d1525af6eb401127b376626d223ff1f74a498401
farkasgabor.mail@gmail.com <farkasgabor.mail@gmail.com@2d66b122-5dff-8467-672b-2567ea858774>
2012-03-19 12:44:56 +0000

    Manifest.mf fájlt kikerült a bin könyvtárból a projekt gyökerébe, így verziókövethető, kezelhető...

--- skeleton/build.bat
+++ skeleton/build.bat
@@ -5,6 +5,6 @@ del bin\engine\*.class /Q
 del bin\*.jar /Q
 javac -classpath src\ -d bin src\engine\*.java src\game\*.java
 cd bin
-jar cmf MANIFEST.mf skeleton.jar game\*.class engine\*.class
+jar cmf ..\MANIFEST.mf skeleton.jar game\*.class engine\*.class
 cd ..
 echo done. bye.
